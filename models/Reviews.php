635bcd412a0da4be11f7341ed0c923375b57d0eb
STAFF\congfeng <congfeng@staff.sina.com.cn>
2018-04-24 14:09:06 +0800

    reviews

---
+++ models/Reviews.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "REVIEWS".
+ *
+ * @property int $REVIEW_ID
+ * @property int $HAS
+ * @property int $WRITES
+ * @property string $REVIEW_DATE
+ * @property string $REVIEW_COMMENT
+ *
+ * @property USERS $wRITES
+ */
+class Reviews extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'REVIEWS';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['REVIEW_ID'], 'required'],
+            [['REVIEW_ID', 'HAS', 'WRITES'], 'integer'],
+            [['REVIEW_DATE'], 'string', 'max' => 7],
+            [['REVIEW_COMMENT'], 'string', 'max' => 1000],
+            [['REVIEW_ID'], 'unique'],
+            [['WRITES'], 'exist', 'skipOnError' => true, 'targetClass' => USERS::className(), 'targetAttribute' => ['WRITES' => 'USER_ID']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'REVIEW_ID' => 'Review  ID',
+            'HAS' => 'Has',
+            'WRITES' => 'Writes',
+            'REVIEW_DATE' => 'Review  Date',
+            'REVIEW_COMMENT' => 'Review  Comment',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getWRITES()
+    {
+        return $this->hasOne(USERS::className(), ['USER_ID' => 'WRITES']);
+    }
+
+    /**
+     * @inheritdoc
+     * @return ReviewsQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new ReviewsQuery(get_called_class());
+    }
+}
--- views/site/userinfo.php
+++ views/site/userinfo.php
@@ -8,21 +8,45 @@ use yii\widgets\ActiveForm;
 /* @var $form ActiveForm */
 ?>
 <div class="site-userinfo">
-    <h2>UserInfo:</h2>
-    <?php $form = ActiveForm::begin(); ?>
-        <?= $form->field($model, 'USER_ID')->input('text',['style'=>'width:30%', 'readonly'=>'true']) ?>
-        <?= $form->field($model, 'PHONE')->input('text',['style'=>'width:30%']) ?>
-        <?= $form->field($model, 'FIRST_NAME')->input('text',['style'=>'width:30%']) ?>
-        <?= $form->field($model, 'LAST_NAME')->input('text',['style'=>'width:30%']) ?>
-        <?= $form->field($model, 'EMAIL')->input('text',['style'=>'width:30%']) ?>
-        <?= $form->field($model, 'GENDER')->input('text',['style'=>'width:30%']) ?>
-        <?= $form->field($model, 'BIRTH_DATE')->input('text',['style'=>'width:30%']) ?>
-        <?= $form->field($model, 'PASSWORD')->input('text',['style'=>'width:30%']) ?>
-        <?= $form->field($model, 'USER_PICTURE')->input('text',['style'=>'width:30%']) ?>
-    
-        <div class="form-group">
-            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
-        </div>
-    <?php ActiveForm::end(); ?>
+    <div class="row">
+        <div class="col-md-6">
+            <h2>UserInfo:</h2>
+            <?php $form = ActiveForm::begin(); ?>
+            <?= $form->field($model, 'USER_ID')->input('text',['readonly'=>'true']) ?>
+            <?= $form->field($model, 'PHONE')->input('text') ?>
+            <?= $form->field($model, 'FIRST_NAME')->input('text') ?>
+            <?= $form->field($model, 'LAST_NAME')->input('text') ?>
+            <?= $form->field($model, 'EMAIL')->input('text') ?>
+            <?= $form->field($model, 'GENDER')->input('text') ?>
+            <?= $form->field($model, 'BIRTH_DATE')->input('text') ?>
+            <?= $form->field($model, 'PASSWORD')->input('text') ?>
+            <?= $form->field($model, 'USER_PICTURE')->input('text') ?>
 
-</div><!-- site-userinfo -->
+            <div class="form-group">
+                <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+            </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+        <div class="col-md-6">
+            <h2>Reviews:</h2>
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th>DATE</th>
+                    <th>COMMENT</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php if (!empty($reviews)){ ?>
+                <?php foreach ($reviews as $review): ?>
+                <tr>
+                    <td><?=$review['DATE'];?></td>
+                    <td><?=$review['COMMENT'];?></td>
+                </tr>
+                <?php endforeach; ?>
+                <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
